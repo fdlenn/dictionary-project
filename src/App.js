d3c2f226906b99ab9452e15022639b053656fef4
fdlenn <101424376+fdlenn@users.noreply.github.com>
2022-09-10 14:49:45 +0100

    Search Engine

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from "./logo.png";
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
@@ -7,6 +8,10 @@ export default function App() {
       <header className="App-header">
       <img src={logo} className="App-logo img-fluid" alt="logo" />
       </header>
+      <main>
+        <Dictionary />
+      </main>
+      <footer className="App-footer"><small>Coded by Laura Fenn</small></footer>
     </div>
   );
 }
