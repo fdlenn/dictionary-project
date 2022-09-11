c7bcc1c3a7839add32cb9b5cc62a4de31ce08ac3
fdlenn <101424376+fdlenn@users.noreply.github.com>
2022-09-11 16:28:13 +0100

    corrected error

--- src/Meaning.js
+++ src/Meaning.js
@@ -5,16 +5,16 @@ import "./Meaning.css";
 export default function Meaning(props) {
 return (
     <div className="Meaning">
-    <h3>{props.meaning.partOfSpeech}</h3>
-    {props.meaning.definitions.map(function (definition, index) {
-      return (
-        <div key={index}>
-      <div className="definition">{definition.definition}</div> 
-      <div className="example">{definition.example}</div>
-      <Synonyms synonyms={definition.synonyms} />
-        </div>
-      );
-    })}
+      <h3>{props.meaning.partOfSpeech}</h3>
+      {props.meaning.definitions.map(function (definition, index) {
+        return (
+          <div key={index}>
+            <div className="definition">{definition.definition}</div> 
+            <div className="example">{definition.example}</div>
+            <Synonyms synonyms={definition.synonyms} />
+          </div>
+       );
+      })}
   </div>
 );
 }
